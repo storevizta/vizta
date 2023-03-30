71c9841490e4b2bb459116cc34672ef5e8670dbd
sebacolladott <105157508+sebacolladott@users.noreply.github.com>
2023-03-30 20:32:20 -0300

    Structure creating

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -1,21 +1,9 @@
-import { createGlobalStyle } from "styled-components";
-
 import { Outlet } from "react-router-dom";
 
 export const Layout = () => {
   return (
     <>
-      <GlobalStyle />
       <Outlet />
     </>
   );
 };
-
-const GlobalStyle = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    /* outline: 1px solid red; */
-    box-sizing: border-box;
-  }
-`;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,15 @@
+import "./main.css";
+
 import React, { StrictMode } from "react";
 
-import { createRoot } from "react-dom/client";
-
-import { store } from "./app/store";
-
-import { Provider } from "react-redux";
+import ReactDOM, { render, createRoot } from "react-dom/client";
 
 import { router } from "./router";
 
 import { RouterProvider } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <StrictMode>
-      <RouterProvider router={router} />
-    </StrictMode>
-  </Provider>
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
 );
