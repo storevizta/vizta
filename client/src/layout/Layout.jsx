a26fd02be10e36b9ff40f05e90ce492264d1c958
sebacolladott <105157508+sebacolladott@users.noreply.github.com>
2023-03-30 23:16:13 -0300

    Structure creating

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -1,9 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
 import { Outlet } from "react-router-dom";
 
 export const Layout = () => {
   return (
     <>
+      <GlobalStyle />
       <Outlet />
     </>
   );
 };
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    /* outline: 1px solid red; */
+    box-sizing: border-box;
+}
+`;
