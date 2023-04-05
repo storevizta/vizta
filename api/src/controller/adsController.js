3e51b7e9682691296ec1fcb1a313e2622816fcd2
bobardou <jeronimoevilar@outlook.com>
2023-04-05 17:12:44 -0300

    routes v1

--- api/src/controller/adsController.js
+++ api/src/controller/adsController.js
@@ -1,44 +1,134 @@
 const htppError = require("../handler/handlerError.js");
+const {Ads, Category} = require("../database.js")
 
-const getPublication = async (req, res) => {
+const getAds = async (req, res) => {
+  const {title} = req.query
   try {
+    if(title){
+      const response = await Ads.findOne({
+        where: {title: title}
+      })
+      res.status(200).json(response)
+    }else{
+      const response = await Ads.findAll()
+      res.status(200).json(response)
+    }
   } catch (e) {
     htppError(res, e);
   }
 };
 
-const getPublicationById = async (req, res) => {
+const getAdsById = async (req, res) => {
+  const {id} = req.params
   try {
+    if(id){
+      const response = await Ads.findOne({
+        where:{id: id}
+      })
+      res.status(200).json(response)
+    }else{
+      throw new Error("Falta el ID")
+    }
   } catch (e) {
     htppError(res, e);
   }
 };
 
-const postPublication = async (req, res) => {
+const postAds = async (req, res) => {
+  const {image, title, price, description, stock, oldPrice, discount, category} = req.body
   try {
+    if(image || title || price || description || stock){
+      const newAds = await Ads.create({
+        image,
+        title, 
+        price, 
+        description, 
+        stock, 
+        oldPrice, 
+        discount
+      })
+
+      const filter = await Category.findOne({
+        where: {name: category}
+      })
+
+      await newAds.setAds(filter)
+
+      const response = await Ads.findOne({
+        where:{id: newAds?.id},
+        include:{
+          model: Category
+        }
+      })
+
+      res.status(200).json(response)
+
+    }else{
+      throw new Error("Missing data")
+    }
   } catch (e) {
     htppError(res, e);
   }
 };
 
-const putPublication = async (req, res) => {
+const getAdsByCategory = async (req,res) => {
+  const {category} = req.params
   try {
+    if(category){
+      const post = await Category.findAll({
+        where: {id: category},
+        include: {model: Ads}
+      })
+
+      res.status(200).json(post)
+    }else{
+      throw new Error("Missing data")
+    }
+  } catch (e) {
+    htppError(res, e);
+  }
+}
+
+const putAds = async (req, res) => {
+  const {changes} = req.body
+  const {id} = req.params
+  try {
+    if(changes && id){
+      const post = await Ads.findOne({
+        where:{id: id}
+      })
+      const response = await post.update({changes})
+      res.status(200).json(response)
+    }else{
+      throw new Error("Missing id or changes")
+    }
   } catch (e) {
     htppError(res, e);
   }
 };
 
-const deletePublication = async (req, res) => {
+const deleteAds = async (req, res) => {
+  const {id} = req.params
   try {
+    if(id){
+      const post = await Ads.findOne({
+        where:{id:id}
+      })
+      const response = post.destroy()
+      res.status(200).json(response)
+    }else{
+      throw new Error("Missing id")
+    }
   } catch (e) {
     htppError(res, e);
   }
 };
 
 module.exports = {
-  getPublication,
-  getPublicationById,
-  postPublication,
-  putPublication,
-  deletePublication,
+  getAds,
+  getAdsById,
+  postAds,
+  putAds,
+  deleteAds,
+  getAdsByCategory
 };
