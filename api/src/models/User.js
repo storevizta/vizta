b6d96701c13a1ffff03110c17db18fa428747a46
sebacolladott <105157508+sebacolladott@users.noreply.github.com>
2023-04-06 15:22:39 -0300

    Seba

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -9,8 +9,8 @@ module.exports = (sequelize) => {
       primaryKey: true,
     },
     name: { type: DataTypes.STRING, allowNull: false },
-    username: { type: DataTypes.STRING, allowNull: false },
-    email: { type: DataTypes.STRING, allowNull: false },
+    // username: { type: DataTypes.STRING, allowNull: false, unique: true },
+    email: { type: DataTypes.STRING, allowNull: false, unique: true },
     password: { type: DataTypes.STRING, allowNull: false },
     address: { type: DataTypes.ARRAY(DataTypes.STRING) },
   });
