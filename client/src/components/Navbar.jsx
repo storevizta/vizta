eea4c0c7975c07efd051d4095e77d1768ed68e1d
Svong <74398777+Hejz-Svong@users.noreply.github.com>
2023-04-06 21:19:53 -0300

    Loadd

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Navbar = () => {
   return (
     <nav>
-      <div>Logo</div>
+      <div>Logos</div>
       <div>Searchbar</div>
       <div>Post</div>
       <div>Sign In</div>
