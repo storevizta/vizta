8fd91b414e73a3dae6b0d70e84dc9a6071cf2970
bobardou <jeronimoevilar@outlook.com>
2023-04-10 13:55:54 -0300

    changes

--- api/src/controller/usersController.js
+++ api/src/controller/usersController.js
@@ -1,6 +1,39 @@
-const getUser = async (req, res) => {};
+require('dotenv').config();
+const { User } = require('../database');
+const bcrypt = require('bcrypt');
 
-const updateUser = async (req, res) => {};
+const getUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    if (id) {
+      const response = await User.findByPk(id);
+      res.status(200).json(response);
+    } else {
+      const response = await User.findAll();
+      res.status(200).json(response);
+    }
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+};
+
+const updateUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    if (id) {
+      const actualUser = await findByPk(id);
+      const hashedPassword = await bcrypt.hash(actualUser.password, 10);
+      actualUser.update({
+        name: actualUser.name,
+        email: actualUser.email,
+        password: hashedPassword,
+        address: actualUser.address,
+      });
+    }
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+};
 
 const deleteUser = async (req, res) => {};
 
