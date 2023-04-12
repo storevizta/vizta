ee9171764c457bcebc95b97ab86fb571a308ab89
Svong <74398777+Hejz-Svong@users.noreply.github.com>
2023-04-12 04:14:25 -0300

    Add Tailwind conf

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}'],
   theme: {
     extend: {},
   },
   plugins: [
     
-    require('@tailwindcss/aspect-ratio'),
+    
   ],
 };
