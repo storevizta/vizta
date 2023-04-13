9b4dcaadf926e98723206ac14a0e621aabfd8ecb
programador5781 <programador5781@gmail.com>
2023-04-12 23:05:00 -0500

    manejo de errores en userController

--- api/src/controller/usersController.js
+++ api/src/controller/usersController.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 
 const { User } = require('../database');
 
+const { 
+  createMissingIdException, 
+  createInvalidInputException 
+} = require('../exceptions/exceptions')
+
 const bcrypt = require('bcrypt');
 
 const getUser = async (req, res) => {
@@ -11,17 +16,31 @@ const getUser = async (req, res) => {
       const response = await User.findByPk(id);
       res.status(200).json(response);
     } else {
-      throw new Error('Missing Id');
+      //throw new Error('Missing Id');
+      throw createMissingIdException('Missing Id')
     }
   } catch (error) {
-    res.status(400).json(error.message);
+    //res.status(400).json(error.message);
+    res.status(error.statusCode).json({ message: error.message });
   }
 };
+
+
+
 
 const updateUser = async (req, res) => {
   const { id } = req.params;
   const { name, email, password, address } = req.body;
   try {
+    if(!id){
+      throw createMissingIdException('Id is missing');
+    }
+    if (name === undefined || email === undefined || password === undefined) {
+      throw createInvalidInputException('Name, email and password are required');
+    }
+    if (email && !isValidEmail(email)) {
+      throw createInvalidInputException('Invalid email format');
+    }
     if (id) {
       const actualUser = await User.findByPk(id);
 
@@ -35,7 +54,28 @@ const updateUser = async (req, res) => {
       res.status(200).json(updated);
     }
   } catch (error) {
-    res.status(400).json(error.message);
+    let statusCode = 500;
+    let errorMessage = 'Internal Server Error';
+    
+    if (error.statusCode) {
+      statusCode = error.statusCode;
+      errorMessage = error.message;
+    } else if (error.name === 'SequelizeUniqueConstraintError') {
+      statusCode = 409;
+      errorMessage = 'Unique constraint error';
+    } else if (error.name === 'MissingIdException') {
+      statusCode = 400;
+      errorMessage = 'Missing Id';
+    } else if (error.name === 'NotFoundException') {
+      statusCode = 404;
+      errorMessage = 'Resource not found';
+    } else if (error.name === 'InvalidInputException') {
+      statusCode = 400;
+      errorMessage = 'Invalid input';
+    }
+    
+    console.error(error);
+    res.status(statusCode).json({ error: errorMessage });
   }
 };
 
---
+++ api/src/exceptions/exceptions.js
@@ -0,0 +1,19 @@
+const createMissingIdException = (message) => {
+    const error = new Error(message);
+    error.name = 'MissingIdException';
+    error.statusCode = 400;
+    return error;
+};
+
+const createInvalidInputException = (message) => {
+    const error = new Error(message);
+    error.name = 'InvalidInputException';
+    error.statusCode = 400;
+    return error;
+  };
+  
+
+module.exports = {
+    createMissingIdException,
+    createInvalidInputException
+};
