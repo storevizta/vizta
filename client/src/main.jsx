eb48179ed9176a9720ebbb7a8703a38720254e67
sebacolladott <105157508+sebacolladott@users.noreply.github.com>
2023-04-13 17:09:32 -0300

    Seba

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React, { StrictMode } from 'react';
 
 import { createRoot } from 'react-dom/client';
 
-import { store } from './reduxToolkit/store/store';
+import { store } from './app/store';
 
 import { Provider } from 'react-redux';
 
