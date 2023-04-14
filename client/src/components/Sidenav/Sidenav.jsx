78a1da05a66312c23f248766ad335bcd4e439ed4
Svong <74398777+Hejz-Svong@users.noreply.github.com>
2023-04-14 16:03:18 -0300

    Fix

--- client/src/components/Sidenav/Sidenav.jsx
+++ client/src/components/Sidenav/Sidenav.jsx
@@ -4,10 +4,7 @@ export const SideNav = () => {
   return (
     <Sidenav>
       <br />
-      <ItemSidenav href="#">Categories</ItemSidenav>
-      <br />
-      <br />
-      <br />
+     
       <ItemSidenav href="/post">Recently posted</ItemSidenav>
       <ItemSidenav href="#">Price</ItemSidenav>
       <ItemSidenav href="#">Category</ItemSidenav>
