f0ed508c01560af7148cfa53cd27dede12a25d3d
MatiasPiris17 <matiaslinkedin17@gmail.com>
2023-04-14 15:19:43 -0300

    detail

--- client/src/pages/Detail.jsx
+++ client/src/pages/Detail.jsx
@@ -1,21 +1,30 @@
-import styled from 'styled-components';
-
 import { useParams } from 'react-router-dom';
 
 import { useGetAdByIdQuery } from '../features/slices/adsSlice';
+
+import Loading from '../components/Loading';
 
 export const Detail = () => {
   const { id } = useParams();
 
   const { data, error, isLoading } = useGetAdByIdQuery(id);
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <div><Loading/></div>;
 
-  const { title } = data;
+  const { title,image,description, stock, price, oldPrice, discount  } = data;
 
   return (
     <>
-      <div>{title}</div>
+    <div key={id}>
+      {/* <img src={image} alt="image not found" /> */}
+      <h1>{title}</h1>
+      <h2>{price}</h2>
+      <h3>{stock}</h3>
+      <h3>{oldPrice}</h3>
+      <h3>{discount}</h3>
+      <h4>{description}</h4>
+    </div>
     </>
   );
 };
+
