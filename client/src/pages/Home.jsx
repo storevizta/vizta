3fac768bbeb562319d3a05914515644612c0ca92
MatiasPiris17 <matiaslinkedin17@gmail.com>
2023-04-14 11:26:10 -0300

    card

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,3 @@
-import styled from 'styled-components';
-
-import { Link } from 'react-router-dom';
-
 import { useGetAdsQuery } from '../features/slices/adsSlice';
 
 import { Navbar } from '../components/Navbar/NavBar';
@@ -10,10 +6,14 @@ import { Sidenav } from '../components/Sidenav';
 
 import { Cards } from '../components/Cards';
 
+import Loading from '../components/Loading';
+
 export const Home = () => {
   const { data, error, isLoading } = useGetAdsQuery();
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <div><Loading/></div>;
+
+  if (error) return <div>Error: {error.message}</div>
 
   return (
     <div>
@@ -21,10 +21,8 @@ export const Home = () => {
       <Sidenav />
       {data &&
         data?.map((el) => (
-          <Link to={`/detail/${el.id}`} key={el.id}>
             <Cards info={el} />
-          </Link>
         ))}
     </div>
   );
-};
+}
