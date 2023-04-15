b8febe42539062235fd41256610883460d76f7aa
Sebastian Eduardo Collado <105157508+sebacolladott@users.noreply.github.com>
2023-04-15 20:24:04 -0300

    Revert "ad detail"

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,36 +1,5 @@
 import styled from 'styled-components';
 
-import { useParams } from 'react-router-dom';
-
-import { Loading } from '../components/Loading';
-
-import { Error } from '../components/Error';
-
-import { useGetUserByIdQuery } from '../features/slices/userSlice';
-
 export const Profile = () => {
-
-    const { id } = useParams();
-    
-    const { data, error, isLoading } = useGetUserByIdQuery(id);
-
-    if (isLoading)
-    return (
-      <div>
-        <Loading />
-      </div>
-    );
-
-  if (error)
-    return (
-      <div>
-        <Error />
-      </div>
-    );
-
-    const {name, address} = data;
-
-  return (<div>
-            <p>{name}</p>
-  </div>);
+  return <div>Profile</div>;
 };
