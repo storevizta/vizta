e1aa75cf1647a524bc4c6e8d89d9a40150a8e315
unknown <caritu_rios@hotmail.com>
2023-04-17 13:18:57 -0300

    post

--- client/src/pages/Post.jsx
+++ client/src/pages/Post.jsx
@@ -24,8 +24,6 @@ export const Post = () => {
     state: '',
   });
 
-  console.log(data);
-
   const [errors, setErrors] = useState({});
 
   const [createAd] = useCreateAdMutation();
@@ -47,6 +45,8 @@ export const Post = () => {
 
     if(data.price === ""){
       errors.price = "The Price is required"
+    } else if (data.price <= 0){
+      errors.price = "The price has to be equal to or greater than 0"
     }
 
     if(data.condition === ""){
@@ -82,6 +82,7 @@ export const Post = () => {
       });
 
     swal('Successful created!');
+
 
     navigate('/home');
     }
@@ -222,6 +223,9 @@ export const Post = () => {
           <button
             className="block mx-auto bg-white hover:bg-zinc-600 px-8 py-2 rounded"
             type="submit"
+            disabled= {
+              !data.title || !data.price || !data.categoryId || !data.condition
+            }
           >
             <p className="font-bold hover:text-white">Submit</p>
           </button>
