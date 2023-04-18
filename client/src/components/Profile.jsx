56c4d97150cd3cccf4bfce063ace8260e0f87a0d
leonelmunter <leonelmunter@gmail.com>
2023-04-18 16:00:26 -0300

    Profile

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -23,6 +23,8 @@ export const Profile = () => {
         nickname: user.nickname,
         email: user.email,
         picture: user.picture,
+        address: user.address,
+        phone: user.phone,
       });
     }
   }, [isAuthenticated, user, createUser]);
@@ -30,9 +32,19 @@ export const Profile = () => {
   return (
     isAuthenticated && (
       <Link to="/profile">
+        <div>
+          <p>General Configuration</p>
+          <p>Advertisements</p>
+          <p>Favorites</p>
+          <p>Subscriptcion</p>
+          <p>Reputation</p>
+        </div>
         <div className="p-2 flex gap-2 hover:bg-zinc-700">
           <img className="w-5" src={user.picture} alt={user.name} />
-          <div className="text-slate-50">{user.name}</div>
+          <p className="text-slate-50">{user.name}</p>
+          <p>{user.email}</p>
+          <p>{user.address}</p>
+          <p>{user.phone}</p>
         </div>
       </Link>
     )
