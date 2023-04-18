b0eda60542a0adb907fa755e0a8c5916f9ddfd58
sebacolladott <105157508+sebacolladott@users.noreply.github.com>
2023-04-18 13:16:36 -0300

    Seba

--- api/src/routes/category.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-const router = require('express').Router();
-
-const { getCategory } = require('../controller/categoryController.js');
-
-router.get('/', getCategory);
-
-module.exports = router;
--- client/src/pages/Landing.jsx
+++
@@ -1,3 +0,0 @@
-export const Landing = () => {
-  return <div>Landing</div>;
-};
