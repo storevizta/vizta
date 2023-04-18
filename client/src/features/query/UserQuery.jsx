1c85df54038a9920aca5588552b4fb897d6d491f
sebacolladott <105157508+sebacolladott@users.noreply.github.com>
2023-04-18 13:40:29 -0300

    Seba

--- client/src/features/query/UserQuery.jsx
+++ client/src/features/query/UserQuery.jsx
@@ -1,20 +1,20 @@
-// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-// export const User = createApi({
-//   reducerPath: 'user',
-//   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001/' }),
-//   endpoints: (builder) => ({
-//     getUserId: builder.query({
-//       query: (id) => `/users/${id}`,
-//     }),
-//     createUser: builder.mutation({
-//       query: (data) => ({
-//         url: '/users',
-//         method: 'POST',
-//         body: data,
-//       }),
-//     }),
-//   }),
-// });
+export const User = createApi({
+  reducerPath: 'user',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001/' }),
+  endpoints: (builder) => ({
+    getUserId: builder.query({
+      query: (id) => `/users/${id}`,
+    }),
+    createUser: builder.mutation({
+      query: (data) => ({
+        url: '/users',
+        method: 'POST',
+        body: data,
+      }),
+    }),
+  }),
+});
 
-// export const { useGetUserIdQuery, useCreateUserMutation } = User;
+export const { useGetUserIdQuery, useCreateUserMutation } = User;
