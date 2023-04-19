7e7183a6fc267b545710bdb5e9523212663d2063
leonelmunter <leonelmunter@gmail.com>
2023-04-19 11:09:01 -0300

    Profile

--- client/src/firebase/config.js
+++ client/src/firebase/config.js
@@ -1,14 +1,14 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 
-import { getStorage } from "firebase/storage";
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA3O8bLqxnqZvGcXz6R5lj1-cLPr72VBbI",
-  authDomain: "vizta-6e742.firebaseapp.com",
-  projectId: "vizta-6e742",
-  storageBucket: "vizta-6e742.appspot.com",
-  messagingSenderId: "852960842650",
-  appId: "1:852960842650:web:8e9284710e278042d36023"
+  apiKey: 'AIzaSyA3O8bLqxnqZvGcXz6R5lj1-cLPr72VBbI',
+  authDomain: 'vizta-6e742.firebaseapp.com',
+  projectId: 'vizta-6e742',
+  storageBucket: 'vizta-6e742.appspot.com',
+  messagingSenderId: '852960842650',
+  appId: '1:852960842650:web:8e9284710e278042d36023',
 };
 
 const app = initializeApp(firebaseConfig);
