7ed05975b6ebfaf0add5c15ab984760970e4b8f5
Nefta Hernandez <neftaligow01@gmail.com>
2023-04-18 19:11:31 -0600

    Error, solo faltan los estilos

--- client/src/components/Error.jsx
+++ client/src/components/Error.jsx
@@ -1,7 +1,9 @@
 export const Error = () => {
   return (
-    <>
-      <div>Error...</div>
-    </>
+    <div>
+      <h1>Error 404</h1>
+      <p>Something went wrong or page not found</p>
+      <p>Reload the page or visit: localhost:3000</p>
+    </div>
   );
 };
