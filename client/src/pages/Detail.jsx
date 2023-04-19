213ab822e567ea9cc3d38ac5e78600e9e5b63154
unknown <caritu_rios@hotmail.com>
2023-04-19 10:37:45 -0300

    detail

--- client/src/pages/Detail.jsx
+++ client/src/pages/Detail.jsx
@@ -1,5 +1,7 @@
 import { useParams } from 'react-router-dom';
 
+import { useState } from 'react';
+
 import { Link } from 'react-router-dom';
 
 import { useGetAdByIdQuery } from '../features/query/AdsQuery';
@@ -12,10 +14,14 @@ import { UserDetail } from '../components/UserDetail';
 
 import imageError from '../assets/imageError.svg';
 
-import atras from '../assets/row-left.svg';
+import rowLeft from '../assets/row-left.svg';
+
+import rowRight from '../assets/row-right.svg';
 
 export const Detail = () => {
   const { id } = useParams();
+
+  const [currentImage, setCurrentImage] = useState(0);
 
   const { data, error, isLoading } = useGetAdByIdQuery(id);
 
@@ -47,19 +53,53 @@ export const Detail = () => {
     UserId,
   } = data;
 
+  const numberImage = image?.length;
+
+  const handlerNextImage = (e) => {
+    setCurrentImage(currentImage === numberImage - 1 ? 0 : currentImage + 1);
+  };
+
+  const handlerPreviousImage = (e) => {
+    setCurrentImage(currentImage === 0 ? numberImage - 1 : currentImage - 1);
+  };
+
   return (
     <div>
       <Link to="/home">
-        <img src={atras} alt="atras" className="pl-5 w-12" />
+        <img src={rowLeft} alt="atras" className="pl-5 w-12" />
       </Link>
 
-      <div className="flex items-stretch w-4/5 m-auto" key={id}>
-        <img
-          src={image || imageError}
-          className="basis-2/3 rounded-md"
-          alt="image not found"
-        />
-        <div className="basis-1/3 pl-15 ml-3 bg-zinc-700 block ml-15 rounded-md">
+      <div className="flex justify-center w-3/4 m-auto">
+        <div className="flex items-stretch">
+          <img
+            className="w-12"
+            src={rowLeft}
+            alt="row-left"
+            onClick={handlerNextImage}
+          />
+          {image.map((image, index) => {
+            return (
+              <div key={index}>
+                {currentImage === index && (
+                  <img
+                    className="w-screen"
+                    src={image}
+                    key={index}
+                    alt="image"
+                  />
+                )}
+              </div>
+            );
+          })}
+          <img
+            className="w-12"
+            src={rowRight}
+            alt="row-right"
+            onClick={handlerPreviousImage}
+          />
+        </div>
+
+        <div className="pl-15 ml-3 bg-zinc-700 block ml-15 rounded-md">
           <h1 className="font-bold text-white pl-5 text-3xl pt-3">{title}</h1>
 
           <div>
