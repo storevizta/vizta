bed93e161a727d3a1f33660b03443f9df3e77a7e
unknown <caritu_rios@hotmail.com>
2023-04-20 13:44:52 -0300

    estilos en message

--- client/src/components/Response.jsx
+++ client/src/components/Response.jsx
@@ -2,11 +2,9 @@ import { useState } from 'react';
 
 import { useResponseMessageMutation } from '../features/query/MessagesQuery';
 
-import { useNavigate } from 'react-router-dom';
-
 import swal from 'sweetalert';
 
-export const Response = ({ id, response }) => {
+export const Response = ({ id, response, updatedAt }) => {
   const [responseMessage] = useResponseMessageMutation();
 
   const [input, setInput] = useState({
@@ -37,17 +35,34 @@ export const Response = ({ id, response }) => {
   return (
     <div>
       {!response ? (
-        <form onSubmit={handleSubmit}>
-          <p>Response: </p>
+        <form
+          className="pt-8 flex flex-col m-auto items-center"
+          onSubmit={handleSubmit}
+        >
+          <p className="text-lg font-bold">Response: </p>
           <textarea
+            className="text-black w-full mb-5 mt-5 border-slate-400 border-2 w-5/6 h-24 p-3 rounded"
             onChange={handleInput}
             value={input.response}
             placeholder="Response..."
           />
-          <button type="submit">Send</button>
+          <button
+            className="p-3 rounded bg-zinc-700 text-white w-40"
+            type="submit"
+          >
+            Send
+          </button>
         </form>
       ) : (
-        <p>{response}</p>
+        <div className="flex justify-between border-solid">
+          <p className="inline">
+            {updatedAt
+              .slice(0, 10)
+              .replace(/^(\d{4})-(\d{2})-(\d{2})$/g, '$3/$2/$1')}
+          </p>
+          <p className="inline">{response}</p>
+          <p className="inline">Denunciar</p>
+        </div>
       )}
     </div>
   );
