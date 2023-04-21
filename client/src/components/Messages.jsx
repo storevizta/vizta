01e0704c0706f595e296b49557d9e53024fcb76c
unknown <caritu_rios@hotmail.com>
2023-04-21 12:53:57 -0300

    message

--- client/src/components/Messages.jsx
+++ client/src/components/Messages.jsx
@@ -52,11 +52,21 @@ export const Messages = ({ adId, userId }) => {
 
             <p className="inline">Denunciar</p>
           </div>
-          <Response
-            id={message.id}
-            response={message.response}
-            updatedAt={message.updatedAt}
-          />
+          {message.response ? (
+            <div className="flex justify-between">
+              <p className="inline">
+                {message.updatedAt
+                  .slice(0, 10)
+                  .replace(/^(\d{4})-(\d{2})-(\d{2})$/g, '$3/$2/$1')}
+              </p>
+
+              <p className="inline pl-3">{message.response}</p>
+
+              <p className="inline">Denunciar</p>
+            </div>
+          ) : (
+            <p className="text-center">No answer now</p>
+          )}
         </div>
       ))}
       <CreateMessage userId={userId} adId={adId} />
