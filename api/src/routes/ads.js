6b8da7ff73ec155302efb41ac245d2b653a3a22e
sebacolladott <105157508+sebacolladott@users.noreply.github.com>
2023-04-29 03:03:16 -0300

    Seba

--- api/src/routes/ads.js
+++ api/src/routes/ads.js
@@ -5,21 +5,24 @@ const router = require('express').Router();
 const {
   getAds,
   getAdById,
+  getSubscribeUserAds,
   createAd,
   setStatusAd,
   updateAd,
-  deleteAd
+  deleteAd,
 } = require('../controller/adsController.js');
 
 router.get('/', getAds);
 
 router.get('/:id', getAdById);
 
+router.get('/subscribeads', getSubscribeUserAds);
+
 router.post('/', createAd);
 
-router.put("/updateAd", updateAd);
+router.put('/updateAd', updateAd);
 
-router.delete("/:id", deleteAd)
+router.delete('/:id', deleteAd);
 
 router.put('/setstatus', setStatusAd);
 
