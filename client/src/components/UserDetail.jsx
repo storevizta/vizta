9a2bbbfe9fc423c50d332bca5aa90c8e881f2a3f
unknown <caritu_rios@hotmail.com>
2023-05-02 14:13:22 -0300

    caro

--- client/src/components/UserDetail.jsx
+++ client/src/components/UserDetail.jsx
@@ -1,5 +1,7 @@
 import { useGetUserIdQuery } from '../features/query/UserQuery';
 
+import { useGetRatingByUserIdQuery } from '../features/query/RatingQuery';
+
 import { Loading } from '../components/Loading';
 
 import { Error } from '../components/Error';
@@ -27,6 +29,28 @@ export const UserDetail = (userId) => {
     );
   }
 
+  const {
+    data: dataRating,
+    errorRating,
+    isLoadingRating,
+  } = useGetRatingByUserIdQuery(id);
+
+  if (errorRating) {
+    return <div>Error</div>;
+  }
+
+  if (isLoadingRating) {
+    return <div>Is Loading</div>;
+  }
+
+  let inicialValue = 0;
+
+  const sumaRating = dataRating.reduce(
+    (acumulator, rating) => acumulator + rating.rating,
+    inicialValue
+  );
+  const promedioRating = Math.round(sumaRating / dataRating.length);
+
   const { name, address, createdAt } = data;
 
   const created = createdAt
@@ -39,6 +63,45 @@ export const UserDetail = (userId) => {
 
       <div className="flex flex-col">
         <p className="text-sm pl-8 text-black">{name}</p>
+
+        <div className="flex gap-2">
+          <div className="flex flex-row pl-8 pt-1">
+            <p className="text-sm text-black">Rating Average:</p>
+
+            <div className="rating rating-sm pl-2">
+              <input
+                type="radio"
+                name="rating-6"
+                className="mask mask-star-2 bg-orange-400"
+                checked={1 === promedioRating}
+              />
+              <input
+                type="radio"
+                name="rating-6"
+                className="mask mask-star-2 bg-orange-400"
+                checked={2 === promedioRating}
+              />
+              <input
+                type="radio"
+                name="rating-5"
+                className="mask mask-star-2 bg-orange-400"
+                checked={3 === promedioRating}
+              />
+              <input
+                type="radio"
+                name="rating-6"
+                className="mask mask-star-2 bg-orange-400"
+                checked={4 === promedioRating}
+              />
+              <input
+                type="radio"
+                name="rating-6"
+                className="mask mask-star-2 bg-orange-400"
+                checked={5 === promedioRating}
+              />
+            </div>
+          </div>
+        </div>
 
         <p className="text-sm pl-8 text-black">
           {address ? (
