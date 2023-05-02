23b6536391ba0ca4d6b04768cf1a4f536993e167
sebacolladott <105157508+sebacolladott@users.noreply.github.com>
2023-05-02 16:10:49 -0300

    Seba

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -17,54 +17,51 @@ import { Pagination } from '../components/Pagination';
 import { Card } from '../components/Card';
 
 export const Home = () => {
-  // const {
-  //   page,
-  //   title,
-  //   category,
-  //   minPrice,
-  //   maxPrice,
-  //   sort,
-  //   discount,
-  //   condition,
-  // } = useSelector((state) => state.filter);
+  const {
+    page,
+    title,
+    category,
+    minPrice,
+    maxPrice,
+    sort,
+    discount,
+    condition,
+  } = useSelector((state) => state.filter);
 
-  // const { data, error, isLoading } = useGetAdsQuery({
-  //   page,
-  //   title,
-  //   category,
-  //   minPrice,
-  //   maxPrice,
-  //   sort,
-  //   discount,
-  //   condition,
-  // });
+  const { data, error, isLoading } = useGetAdsQuery({
+    page,
+    title,
+    category,
+    minPrice,
+    maxPrice,
+    sort,
+    discount,
+    condition,
+  });
 
-  // if (isLoading) return <Loading />;
+  if (isLoading) return <Loading />;
 
-  // if (error) return <Error />;
+  if (error) return <Error />;
 
   return (
-    <>
-      Home
-      {/* <div className="h-full flex flex-grow">
-        <Sidebar />
-        <div className="w-full p-5">
-          <Featured />
-          <Pagination items={data.length} />
-          {data && data.ads.length === 0 ? (
-            <p>No results found.</p>
-          ) : (
-            <div className="grid grid-cols-5">
-              {data &&
-                data.ads.map((el) => (
-                  <div key={el.id} className="m-3">
-                    <Card info={el} />
-                  </div>
-                ))}
-            </div>
-          )}
-        </div>
-      </div> */}
-    </>
+    <div className="h-full flex flex-grow">
+      <Sidebar />
+      <div className="w-full p-5">
+        <Featured />
+        <Pagination items={data.length} />
+        {data && data.ads.length === 0 ? (
+          <p>No results found.</p>
+        ) : (
+          <div className="grid grid-cols-5">
+            {data &&
+              data.ads.map((el) => (
+                <div key={el.id} className="m-3">
+                  <Card info={el} />
+                </div>
+              ))}
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
